5b027c52a79eea25934c49cfc241175e11e119fd
yahya-mohammed07 <goldroger300@gmail.com>
2021-08-18 16:35:16 +0300

    USING FIND() AND OTHERS

--- main.cpp
+++ main.cpp
@@ -8,10 +8,16 @@ auto main() -> int
   std::puts("");
   std::cout << nums.max() << '\n';
   std::cout << nums.min() << '\n';
-  std::cout << nums.contains(22) << '\n';
+  std::cout << nums.contains(5) << '\n';
   std::cout << nums.depth(5) << '\n';
-  std::cout << *nums.find(5) << '\n';
+
+  if ( nums.find(55) != nums.end() ) {
+    std::cout << *nums.find(55) << '\n';
+  }
   std::cout << *nums.begin() << '\n';
   nums.remove(5);
   nums.print(2);
+  bst_<float> nums_temp = nums;
+  nums_temp.print(3);
+  std::puts("");
 }
