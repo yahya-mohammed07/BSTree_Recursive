14281b9d4fdc2415648cef0081684e7910621d4b
yahya-mohammed07 <goldroger300@gmail.com>
2021-08-19 12:45:32 +0300

    APOLOGY SYSTEM ON SEPERATE FILE

---
+++ lib/apology.hpp
@@ -0,0 +1,40 @@
+#ifndef APOLOGY_HPP
+#define APOLOGY_HPP
+
+#include <unordered_map>
+#include <string_view>
+#include <cassert>
+#include <iostream>
+
+
+enum class Apology {
+  empty = 1,
+  not_found = 2,
+  invalid_order = 3,
+};
+
+inline
+const std::unordered_map<Apology, std::string_view>
+  Apologies =
+{
+  { Apology::empty, "- Apology: Tree is empty...\n" },
+  { Apology::not_found, "- Apology: Node not found...\n" },
+  { Apology::invalid_order, " -Apology: Invalid order...\n"}
+};
+
+inline
+auto get_apology(const Apology& apology)
+    -> void
+{
+  if ( apology == Apology::empty ) {
+    std::cerr << Apologies.at( Apology::empty );
+    assert(false);
+  } else if (apology == Apology::invalid_order) {
+    std::cerr << Apologies.at( Apology::invalid_order );
+    assert(false);
+  }
+  std::cerr << Apologies.at( Apology::not_found );
+  assert(false);
+}
+
+#endif // APOLOGY_HPP
