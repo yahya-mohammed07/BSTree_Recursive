1b3d61af3094ff7c0d734a82c47600fa0ef4726c
yahya-mohammed07 <goldroger300@gmail.com>
2021-08-20 16:42:22 +0300

    MORE APOLOGIES ADDED

--- lib/apology.hpp
+++ lib/apology.hpp
@@ -11,6 +11,7 @@ enum class Apology {
   empty = 1,
   not_found = 2,
   invalid_order = 3,
+  invalid_node = 4,
 };
 
 inline
@@ -19,7 +20,8 @@ const std::unordered_map<Apology, std::string_view>
 {
   { Apology::empty, "- Apology: Tree is empty...\n" },
   { Apology::not_found, "- Apology: Node not found...\n" },
-  { Apology::invalid_order, " -Apology: Invalid order...\n"}
+  { Apology::invalid_order, "- Apology: Invalid order...\n"},
+  { Apology::invalid_node, "- Apology: Invalid node...\n"}
 };
 
 inline
@@ -29,8 +31,11 @@ auto get_apology(const Apology& apology)
   if ( apology == Apology::empty ) {
     std::cerr << Apologies.at( Apology::empty );
     assert(false);
-  } else if (apology == Apology::invalid_order) {
+  } else if ( apology == Apology::invalid_order ) {
     std::cerr << Apologies.at( Apology::invalid_order );
+    assert(false);
+  } else if ( apology == Apology::invalid_node ) {
+    std::cerr << Apologies.at( Apology::invalid_node );
     assert(false);
   }
   std::cerr << Apologies.at( Apology::not_found );
