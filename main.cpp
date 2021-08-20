a14e585d16618d0115fa66b1d3483fb40410594e
yahya-mohammed07 <goldroger300@gmail.com>
2021-08-20 16:46:45 +0300

    NEW EXAMPLES

--- main.cpp
+++ main.cpp
@@ -5,19 +5,19 @@ auto main() -> int
 {
   bst_<float> nums { 0, 3, 2, 5, 77, 33, -1, 88, 4, 1, -2, 0.5 };
   nums.print(3);
-  std::puts("");
+  nums.remove(33);
+  // nums.destroy_tree(); // FIXME: it's private because it causes leaks
+  std::cout << '\n' << *nums.begin() << '\n';
   std::cout << nums.max() << '\n';
   std::cout << nums.min() << '\n';
   std::cout << nums.contains(5) << '\n';
   std::cout << nums.depth(5) << '\n';
+  std::cout << *nums.get_parent(77) << '\n';
 
-  if ( nums.find(55) != nums.end() ) {
-    std::cout << *nums.find(55) << '\n';
+  if ( nums.find(5) != nums.end() ) {
+    std::cout << *(++nums.find(5)) << '\n';
   }
-  std::cout << *nums.begin() << '\n';
   nums.remove(5);
   nums.print(2);
-  bst_<float> nums_temp = nums;
-  nums_temp.print(3);
   std::puts("");
 }
