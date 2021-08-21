877c99261a489895b775c1d4ddba8f71f633a587
yahya-mohammed07 <goldroger300@gmail.com>
2021-08-21 13:20:08 +0300

    USING `OPERATOR++()`

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
-#include <iostream>
 #include "lib/bst.hpp"
+#include <iostream>
 
 auto main() -> int
 {
-  bst_<float> nums { 0, 3, 2, 5, 77, 33, -1, 88, 4, 1, -2, 0.5 };
+  bst_<float> nums{0, 3, 2, 5, 77, 33, -1, 88, 4, 1, -2, 0.5};
   nums.print(3);
   nums.remove(33);
-  // nums.destroy_tree(); // FIXME: it's private because it causes leaks
   std::cout << '\n' << *nums.begin() << '\n';
   std::cout << nums.max() << '\n';
   std::cout << nums.min() << '\n';
@@ -14,7 +13,7 @@ auto main() -> int
   std::cout << nums.depth(5) << '\n';
   std::cout << *nums.get_parent(77) << '\n';
 
-  if ( nums.find(5) != nums.end() ) {
+  if (nums.find(5) != nums.end()) {
     std::cout << *(++nums.find(5)) << '\n';
   }
   nums.remove(5);
