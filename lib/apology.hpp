eb25ddfcb3bb60e394463bcbfdf4b6dc8449da86
yahya-mohammed07 <goldroger300@gmail.com>
2021-08-22 18:08:50 +0300

    `SWITCH CASE` ADDED, `SHOW()` ADDED

--- lib/apology.hpp
+++ lib/apology.hpp
@@ -12,34 +12,46 @@ enum class Apology {
   not_found = 2,
   invalid_order = 3,
   invalid_node = 4,
+  invalid_apology = 5,
 };
 
-inline
+inline static
 const std::unordered_map<Apology, std::string_view>
   Apologies =
 {
   { Apology::empty, "- Apology: Tree is empty...\n" },
   { Apology::not_found, "- Apology: Node not found...\n" },
   { Apology::invalid_order, "- Apology: Invalid order...\n"},
-  { Apology::invalid_node, "- Apology: Invalid node...\n"}
+  { Apology::invalid_node, "- Apology: Invalid node...\n"},
+  { Apology::invalid_apology, "- Apology: Invalid apology...\n"}
 };
 
 inline
-auto get_apology(const Apology& apology)
+auto show(const Apology& apology)
     -> void
 {
-  if ( apology == Apology::empty ) {
-    std::cerr << Apologies.at( Apology::empty );
-    assert(false);
-  } else if ( apology == Apology::invalid_order ) {
-    std::cerr << Apologies.at( Apology::invalid_order );
-    assert(false);
-  } else if ( apology == Apology::invalid_node ) {
-    std::cerr << Apologies.at( Apology::invalid_node );
-    assert(false);
+  switch ( apology ) {
+    case Apology::empty: {
+      std::cerr << Apologies.at( Apology::empty );
+      assert(false);
+      break;
+    } case Apology::invalid_order: {
+      std::cerr << Apologies.at( Apology::invalid_order );
+      assert(false);
+      break;
+    } case Apology::invalid_node: {
+      std::cerr << Apologies.at( Apology::invalid_node );
+      assert(false);
+    } case Apology::not_found: {
+      std::cerr << Apologies.at( Apology::not_found );
+      assert(false);
+      break;
+    }
+    default: {
+      std::cerr << Apologies.at( Apology::invalid_apology );
+      assert(false);
+    }
   }
-  std::cerr << Apologies.at( Apology::not_found );
-  assert(false);
 }
 
 #endif // APOLOGY_HPP
