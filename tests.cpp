99d5a20fd748c67899f704e87dbe5825622261e5
yahya-mohammed07 <goldroger300@gmail.com>
2021-08-22 09:58:28 +0300

    Catch2 TESTS

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,5 @@
 #include "lib/bst.hpp"
-#include <catch2/catch_all.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 bst_<float> nums{0, 3, 2, 5, 77, 33, -1, 88, 4, 1, -2, 0.5};
 
@@ -31,11 +31,15 @@ TEST_CASE("get parent") {
 TEST_CASE("find") {
   REQUIRE(*nums.find(88) == 88);
   REQUIRE(*nums.find(3) == 3);
+  REQUIRE(nums.find(22) == nums.end());
 }
 
 TEST_CASE("begin") { REQUIRE(*nums.begin() == -2); }
 
-TEST_CASE("operator()++") { REQUIRE(*(++nums.find(5)) == 33); }
+TEST_CASE("operator()++ & operator(int)++") {
+  REQUIRE(*(++nums.find(5)) == 33);
+  REQUIRE(*(nums.find(5)++) == 33);
+}
 
 TEST_CASE("make empty") {
   nums.burn_tree();
